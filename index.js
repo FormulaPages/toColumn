bafe8d2760c6ca02875db07b894d752c5d63c962
Peter Moresi <peter.moresi@gmail.com>
2015-09-12 14:12:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["exports", "module"], factory);
+    } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
+        factory(exports, module);
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, mod);
+        global.toColumn = mod.exports;
+    }
+})(this, function (exports, module) {
+    "use strict";
+
+    module.exports = toColumn;
+
+    var toColumnMem = [];
+
+    function toColumn(column_index) {
+
+        if (toColumnMem.hasOwnProperty(column_index)) {
+            return toColumnMem[column_index];
+        }
+
+        // The column is determined by applying a modified Hexavigesimal algorithm.
+        // Normally BA follows Z but spreadsheets count wrong and nobody cares.
+
+        // Instead they do it in a way that makes sense to most people but
+        // is mathmatically incorrect. So AA follows Z which in the base 10
+        // system is like saying 01 follows 9.
+
+        // In the least significant digit
+        // A..Z is 0..25
+
+        // For the second to nth significant digit
+        // A..Z is 1..26
+
+        var converted = "",
+            secondPass = false,
+            remainder,
+            value = Math.abs(column_index);
+
+        do {
+            remainder = value % 26;
+
+            if (secondPass) {
+                remainder--;
+            }
+
+            converted = String.fromCharCode(remainder + "A".charCodeAt(0)) + converted;
+            value = Math.floor((value - remainder) / 26);
+
+            secondPass = true;
+        } while (value > 0);
+
+        toColumnMem[column_index] = converted;
+        return converted;
+    }
+});
